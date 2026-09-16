47a07b825f6dfefb3135db4ecc44895d8aa36d41
Kostik <mrpipiskin.only@gmail.com>
2026-09-16 16:10:04 +0300

    Created a new timer

--- lab1-timer/TimerApp.java
+++ lab1-timer/TimerApp.java
@@ -24,3 +24,30 @@ class GoSignal extends TimerTask {
         });
     }
 }
+
+// ================================================
+// PART: KIRILLOV - Repeating countdown every second
+// ================================================
+class CountdownTick extends TimerTask {
+    private int secondsElapsed = 0;
+    private final JTextArea output;
+    private final JLabel timerLabel;
+    private final JLabel statusLabel;
+
+    public CountdownTick(JTextArea output, JLabel timerLabel, JLabel statusLabel) {
+        this.output = output;
+        this.timerLabel = timerLabel;
+        this.statusLabel = statusLabel;
+    }
+
+    @Override
+    public void run() {
+        secondsElapsed++;
+        Toolkit.getDefaultToolkit().beep();
+        SwingUtilities.invokeLater(() -> {
+            timerLabel.setText(String.format("T+%02d", secondsElapsed));
+            statusLabel.setText("COUNTDOWN IN PROGRESS");
+            output.append("[Countdown] T+" + secondsElapsed + "s ...\n");
+        });
+    }
+}
